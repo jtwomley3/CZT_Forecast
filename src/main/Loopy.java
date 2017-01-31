b940304022c976a986dcb27e0c83d9fb1e3b0ab2
Twomley <JARED_M_TWOMLEY@homedepot.com>
2017-01-31 12:09:29 -0500

    Initial Commit

---
+++ src/main/Loopy.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Loopy {
+
+	public static void main(String[] args) {
+		System.out.println("Before the Loop.");
+		
+		int i = 0;
+		
+		while (i < 4){
+			System.out.println("iteration currently i = " + i);
+			
+			i++;
+		}
+		
+		System.out.println("After the Loop.");
+		
+	}
+
+}
