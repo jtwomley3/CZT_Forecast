8e22ee443c93bb234341e816f30200524305cdd0
Twomley <JARED_M_TWOMLEY@homedepot.com>
2017-03-02 13:39:08 -0500

    added the check for multiple winners, launcher continues until multiple winners are found

--- src/guessingGame/GuessingGame.java
+++ src/guessingGame/GuessingGame.java
@@ -1,5 +1,7 @@
 package guessingGame;
 
+import java.util.ArrayList;
+
 public class GuessingGame {
 	
 	Player p1 = new Player();
@@ -9,7 +11,18 @@ public class GuessingGame {
 	boolean p1Correct = false;
 	boolean p2Correct = false;
 	boolean p3Correct = false;
+	ArrayList<Integer> winner = new ArrayList<Integer>();
 	
+	public int getWinnerCount(){
+		return winner.size();
+	}
+	
+	public void clearWinnerList(){
+		winner.clear();
+		p1Correct = false;
+		p2Correct = false;
+		p3Correct = false;
+	}
 	
 	public void startGame(){
 		System.out.println("Let the game begin!");
@@ -27,9 +40,9 @@ public class GuessingGame {
 			int p2Guess = p2.getPlayerGuess();
 			int p3Guess = p3.getPlayerGuess();
 			
-			System.out.println("Player 1 guessed " + p1Guess);
-			System.out.println("Player 2 guessed " + p2Guess);
-			System.out.println("Player 3 guessed " + p3Guess);
+//			System.out.println("Player 1 guessed " + p1Guess);
+//			System.out.println("Player 2 guessed " + p2Guess);
+//			System.out.println("Player 3 guessed " + p3Guess);
 			
 			if (p1Guess == answer){	
 				p1Correct = true;
@@ -46,9 +59,9 @@ public class GuessingGame {
 			if (p3Correct == true || p2Correct == true || p1Correct == true){
 				System.out.println("We have a winner!");
 				
-				System.out.println("Player 1's guess was correct? - " + p1Correct);
-				System.out.println("Player 2's guess was correct? - " + p2Correct);
-				System.out.println("Player 3's guess was correct? - " + p3Correct);
+//				System.out.println("Player 1's guess was correct? - " + p1Correct);
+//				System.out.println("Player 2's guess was correct? - " + p2Correct);
+///				System.out.println("Player 3's guess was correct? - " + p3Correct);
 				System.out.println("The game is Over.");
 				break;
 			}
@@ -58,4 +71,39 @@ public class GuessingGame {
 		}
 	}
 
+	
+	public void checkWinner(){
+		
+		
+		if (p3Correct == true){
+			winner.add(3);
+		};
+		if (p2Correct == true){
+			winner.add(2);
+		};
+		if (p1Correct == true){
+			winner.add(1);
+		};
+		
+		if (winner.size() > 1){
+			System.out.println("There were multiple winners...");
+			System.out.print("They are players ");
+			
+			for  (int i=1; i <= winner.size(); i++){
+				if (i == 1){
+					System.out.print(winner.get(i));
+				
+				}
+				else {
+					System.out.print(", " + i);
+				}
+			}
+			
+			System.out.println(".");
+			System.out.println("Congratulations!");
+		}
+		else {
+			System.out.println("The winner was Player " + winner.get(0));
+		}
+	}
 }
--- src/guessingGame/GuessingGameLauncher.java
+++ src/guessingGame/GuessingGameLauncher.java
@@ -6,7 +6,18 @@ public class GuessingGameLauncher {
 	public static void main (String[] args){
 	
 	GuessingGame newgame = new GuessingGame();
+	
+	while (true){
 	newgame.startGame();
+	newgame.checkWinner();
+	
+		if (newgame.getWinnerCount() > 1){
+			break;
+		}
+		
+	newgame.clearWinnerList();
+	}
+	
 
 	// This line does nothing...literally	
 	
