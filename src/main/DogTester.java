50c788a97bdfb79715b862b8d15a11b2a27cf969
Twomley <JARED_M_TWOMLEY@homedepot.com>
2017-03-02 11:30:34 -0500

    first commit

--- src/main/DogTester.java
+++ src/main/DogTester.java
@@ -5,14 +5,24 @@ public class DogTester {
 	public static void main (String[] args){
 		Dog d = new Dog();
 		int dogSize;
+		String dogName;
+		String dogBreed;
+				
 		
 		d.size = 45;
+		d.Breed = "Daschund";
+		d.Name = "Rover";
+		
 		
 		d.bark();
 		
 		dogSize = d.getSize();
+		dogName = d.getName();
+		dogBreed = d.getBreed();
 		
 		System.out.println("The dog's Size is " + dogSize);
+		System.out.println("The dog's name is " + dogName);
+		System.out.println("Who knows the dog's breed?  It's a " + dogBreed);
 	}
 
 }
